701a1c896cd4017e5c8065a5a086fc109c2399ab
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-08-24 16:28:09 +0100

    spread problme 1

--- spread/problems/01.js
+++ spread/problems/01.js
@@ -1,12 +1,12 @@
 // Write a function that takes a list of numbers, and returns true if the first number is divisible by all of the others.
 // The function may be called with a variable number of parameters
-//
-// function isDivisible(10, 2, 5)
-// > true
-//
-// function isDivisible(10, 5, 3)
-// > false
-//
-// function isDivisible(150, 15, 10, 5, 3, 1, 6, 75, 50, 150, 30, 25, 2)
-// > true
-//
+
+function isDivisible(10, 2, 5)
+> true
+
+function isDivisible(10, 5, 3)
+> false
+
+function isDivisible(150, 15, 10, 5, 3, 1, 6, 75, 50, 150, 30, 25, 2)
+> true
+
